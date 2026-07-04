39c904f80808103941c1a334f8993f700b5f585b
willi19 <willi19@snu.ac.kr>
2026-07-04 16:15:01 +0900

    Keep tabletop labels upright

--- docs/js/audit.js
+++ docs/js/audit.js
@@ -1,5 +1,5 @@
 const HF_BASE = 'https://huggingface.co/datasets/willi19/object_processing/resolve/main/';
-const DATA_VERSION = '20260704-willi19-9aaa4ce-thumbnail-poses-v26';
+const DATA_VERSION = '20260704-willi19-9aaa4ce-label-upright-v27';
 const REVIEW_DB_KEY = 'object_processing.audit.review_versions.v1';
 const REVIEW_DRAFT_KEY = 'object_processing.audit.review_draft.v1';
 const REVIEW_MANIFEST_PATH = 'reviews/manifest.json';
@@ -1081,8 +1081,8 @@ class AuditScene {
       width: maxExtent * 0.68,
       height: maxExtent * 0.28,
       sideOrientation: BABYLON.Mesh.DOUBLESIDE,
-      frontUVs: new BABYLON.Vector4(0, 1, 1, 0),
-      backUVs: new BABYLON.Vector4(1, 1, 0, 0),
+      frontUVs: new BABYLON.Vector4(0, 0, 1, 1),
+      backUVs: new BABYLON.Vector4(1, 0, 0, 1),
     }, this.scene);
     label.material = material;
     label.position = new BABYLON.Vector3(dx, dy, maxExtent * 0.92);
